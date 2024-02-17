601fecdf68ef7c02710898f3c2aa3280397031c6
Suraj Kumar <surajsinhaking77@gmail.com>
2024-02-17 23:12:57 +0530

    done

--- client/src/common/Store/authStore.ts
+++ client/src/common/Store/authStore.ts
@@ -1,17 +1,17 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useState , FC} from "react";
+import React from "react";
 interface AuthContextType {
   userData: User | null;
   setUserData: (newUserData: User | null) => void;
 }
 
-// const AuthContext = createContext<AuthContextType>();
+const AuthContext = createContext<AuthContextType | null>(null);
 
 interface User {
   // Define the properties of your User object here
 }
 
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
+const AuthProvider : FC<{children: React.ReactNode}> = ({ children }) => {
   const [userData, setUserData] = useState<User | null>(null);
 
   return (
