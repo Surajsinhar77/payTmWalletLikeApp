e203792faca5127c7f9105c8b2edaf01a12e585f
suraj kumar <surajsinhakingr77>
2024-03-07 20:15:01 +0530

    done

--- client/src/components/customUi/CardWithForm.tsx
+++ client/src/components/customUi/CardWithForm.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
+// import * as React from "react";
 
 import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
